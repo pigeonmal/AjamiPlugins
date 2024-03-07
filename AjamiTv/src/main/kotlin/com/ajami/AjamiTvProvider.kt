5eb66176c572b3face0e65444862be905f88b525
pigeonmal <eliastafnakji1@gmail.com>
2024-03-07 15:38:52 +0100

    v1.1

--- AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
+++ AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
@@ -3,7 +3,8 @@ package com.ajami
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
-
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
 
 class AjamiTvProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://oha.to/play/"
@@ -19,7 +20,7 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val channels = channelsMap[request.data];
-        val livesList = channels.map { channel ->
+        val livesList = channels?.map { channel ->
             LiveSearchResponse(
             channel.name,
             channel.toString(),
@@ -82,6 +83,6 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
 
         val BeinPremiumChannels = listOf(Channel("BEIN SPORTS 1 PREMIUM", 645242569, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS PREMIUM 1 HD LOCAL", 3290791558, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM 1080 LOCAL", 4171759985, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM 4K H265", 798204953, "Arabia", premiumBein1PosterUrl),Channel("BEINSPORT AR 1 PREMIUM", 3676879039, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS PREMIUM 1 FHD", 344625374, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM HEVC", 3509857289, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM (BACKUP)", 854273020, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 2 PREMIUM 4K H265", 1418536698, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 2 FHD", 755477531, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM 1080 LOCAL", 2696325209, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM", 2613047815, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM HEVC", 1001582955, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 2 HD LOCAL", 3091487581, "Arabia", premiumBein2PosterUrl),Channel("BEINSPORT AR 2 PREMIUM", 1726141553, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM (BACKUP)", 194217660, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 3 FHD", 981252184, "Arabia", premiumBein3PosterUrl),Channel("BEINSPORT AR 3 PREMIUM", 3144945140, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM 1080 LOCAL", 563334526, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM", 1177103234, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS PREMIUM 3 HD LOCAL", 625719851, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM HEVC", 3571468170, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM 4K H265", 3411518820, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM (BACKUP)", 482075260, "Arabia", premiumBein3PosterUrl))
         
-        const val channelsMap = mapOf("bp" to BeinPremiumChannels)
+        val channelsMap = mapOf("bp" to BeinPremiumChannels)
     }
 }
