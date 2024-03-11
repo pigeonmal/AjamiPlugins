4b2b029224d5e5444e6eb3c05106c22c153cac3a
pigeonmal <eliastafnakji1@gmail.com>
2024-03-11 23:24:09 +0100

    Test ??

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        apk("com.lagradost:cloudstream3:pre-release")
+        apk("com.github.pigeonmal:cloudstream:pre-release")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
