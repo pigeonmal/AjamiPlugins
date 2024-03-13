146befdfb6de70a3e8f19797fa0883f94cf30bd2
pigeonmal <eliastafnakji1@gmail.com>
2024-03-13 22:41:56 +0100

    ddd

--- AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
+++ AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
@@ -11,6 +11,7 @@ import com.lagradost.cloudstream3.ui.player.LinkGenerator
 import com.lagradost.cloudstream3.CommonActivity.activity
 import com.lagradost.cloudstream3.utils.UIHelper.navigate
 import com.lagradost.cloudstream3.R
+import com.fasterxml.jackson.annotation.JsonProperty
 
 
 class AjamiTvProvider : MainAPI() { // all providers must be an instance of MainAPI
@@ -19,16 +20,35 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
     override val hasDownloadSupport = false
     override val supportedTypes = setOf(TvType.Live)
     override var lang = "ar"
-    override val hasMainPage = false
+    override val hasMainPage = true
+    var channelsList: List<Channel> = listOf()
 
+    data class wantedChannel(
+        val group: String,
+        val name: String,
+        val poster: String,
+    )
     companion object {
-        val premiumBein1PosterUrl = "https://media.discordapp.net/attachments/1052649740732481569/1215345348818636820/beinpremium1.png?ex=65fc698f&is=65e9f48f&hm=78a133a0068ff47ab9176877bed0608aa013529367030a6656608de6480e293b&=&format=png&quality=lossless"
-        val premiumBein2PosterUrl = "https://media.discordapp.net/attachments/1052649740732481569/1215346089889366016/beinpremium1.png?ex=65fc6a3f&is=65e9f53f&hm=6685ca814e33ecb5c48fd35bec8daf55485e748f89d3cdbabd3f156cf5e96f31&=&format=png&quality=lossless"
-        val premiumBein3PosterUrl = "https://media.discordapp.net/attachments/1052649740732481569/1215346409835077765/beinpremium1.png?ex=65fc6a8c&is=65e9f58c&hm=3f8ba9140d55c59528cefa6f603415da735fe1caaa518c598283b009f6cee53f&=&format=png&quality=lossless"
+        val wantedChannels = listOf(
+            wantedChannel("bp", "BEIN SPORTS PREMIUM 1 FHD |D", "https://media.discordapp.net/attachments/1052649740732481569/1215345348818636820/beinpremium1.png?ex=65fc698f&is=65e9f48f&hm=78a133a0068ff47ab9176877bed0608aa013529367030a6656608de6480e293b&=&format=png&quality=lossless"),
+            wantedChannel("bp", "BEIN SPORTS PREMIUM 2 FHD |D", "https://media.discordapp.net/attachments/1052649740732481569/1215346089889366016/beinpremium1.png?ex=65fc6a3f&is=65e9f53f&hm=6685ca814e33ecb5c48fd35bec8daf55485e748f89d3cdbabd3f156cf5e96f31&=&format=png&quality=lossless"),
+            wantedChannel("bp", "BEIN SPORTS PREMIUM 3 FHD |D", "https://media.discordapp.net/attachments/1052649740732481569/1215346409835077765/beinpremium1.png?ex=65fc6a8c&is=65e9f58c&hm=3f8ba9140d55c59528cefa6f603415da735fe1caaa518c598283b009f6cee53f&=&format=png&quality=lossless")
+            ) 
+    }
 
-        val BeinPremiumChannels = listOf(Channel("BEIN SPORTS 1 PREMIUM", 645242569, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS PREMIUM 1 HD LOCAL", 3290791558, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM 1080 LOCAL", 4171759985, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM 4K H265", 798204953, "Arabia", premiumBein1PosterUrl),Channel("BEINSPORT AR 1 PREMIUM", 3676879039, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS PREMIUM 1 FHD", 344625374, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM HEVC", 3509857289, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 1 PREMIUM (BACKUP)", 854273020, "Arabia", premiumBein1PosterUrl),Channel("BEIN SPORTS 2 PREMIUM 4K H265", 1418536698, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 2 FHD", 755477531, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM 1080 LOCAL", 2696325209, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM", 2613047815, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM HEVC", 1001582955, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 2 HD LOCAL", 3091487581, "Arabia", premiumBein2PosterUrl),Channel("BEINSPORT AR 2 PREMIUM", 1726141553, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS 2 PREMIUM (BACKUP)", 194217660, "Arabia", premiumBein2PosterUrl),Channel("BEIN SPORTS PREMIUM 3 FHD", 981252184, "Arabia", premiumBein3PosterUrl),Channel("BEINSPORT AR 3 PREMIUM", 3144945140, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM 1080 LOCAL", 563334526, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM", 1177103234, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS PREMIUM 3 HD LOCAL", 625719851, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM HEVC", 3571468170, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM 4K H265", 3411518820, "Arabia", premiumBein3PosterUrl),Channel("BEIN SPORTS 3 PREMIUM (BACKUP)", 482075260, "Arabia", premiumBein3PosterUrl))
-        
-        val channelsMap = mapOf("bp" to BeinPremiumChannels)
+
+    override fun init() {
+        val wantedChannelNames = wantedChannels.map { it.name }
+
+        const listOhaChannels: List<OhaChannel> = app.get("https://oha.to/channels").parsedSafe<List<OhaChannel>>()
+        channelsList = listOhaChannels
+                .filter { it.name in wantedChannelNames }
+                .map { ohaChannel ->
+                    val wantedChannel = wantedChannels.find { it.name == ohaChannel.name }
+                    wantedChannel?.let { wantedChannel ->
+                      Channel(ohaChannel.name, ohaChannel.id, ohaChannel.country, wantedChannel.poster, wantedChannel.group)
+                    }
+                }
     }
 
     override val mainPage = mainPageOf(
@@ -36,7 +56,7 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
     )
 
   override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-    val channels = channelsMap[request.data]
+    val channels = channelsList.filter { it.group == request.data }
     val livesList: List<LiveSearchResponse>? = channels?.map { channel ->
         LiveSearchResponse(
             channel.name,
@@ -54,7 +74,7 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
 
     override suspend fun load(url: String): LoadResponse {
         val channelData = parseJson<Channel>(url)
-        val idlive = channelData.id.toString()
+        val idlive = channelData.id
          activity?.navigate(
                 R.id.global_to_navigation_player,
                 GeneratorPlayer.newInstance(
@@ -81,7 +101,7 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         val channelData = parseJson<Channel>(data)
-        val id = channelData.id.toString()
+        val id = channelData.id
 
         callback.invoke(
             ExtractorLink(
@@ -98,10 +118,19 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
 
     data class Channel(
         val name: String,
-        val id: Long,
+        val id: String,
         val country: String,
-        val poster: String
+        val poster: String,
+        val group: String
     )
+
+    data class OhaChannel(
+        @JsonProperty("country") val country: String? = null,
+        @JsonProperty("id") val id: String? = null,
+        @JsonProperty("name") val name: String? = null,
+    )
+
+
 
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
