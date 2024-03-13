17f3e3d0b570fd8721f705dcb6920adf1bf41a62
pigeonmal <eliastafnakji1@gmail.com>
2024-03-13 23:44:22 +0100

    sssd

--- AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
+++ AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
@@ -45,7 +45,7 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
     if (channelsList.isEmpty()) {
         val wantedChannelNames = wantedChannels.map { it.name }
 
-        val listOhaChannels:List<OhaChannel> = app.get("https://oha.to/channels").parsedSafe<OhaChannel::class>() ?: emptyList()
+        val listOhaChannels:List<OhaChannel> = app.get("https://oha.to/channels").parsedSafe<List<OhaChannel>>() ?: emptyList()
              channelsList = listOhaChannels
                 .filter { it.name in wantedChannelNames }
                 .mapNotNull { ohaChannel ->
