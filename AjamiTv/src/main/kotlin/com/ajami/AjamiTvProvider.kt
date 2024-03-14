caee4ce3223139e20a9174e6c761cdd03ba3edb7
pigeonmal <eliastafnakji1@gmail.com>
2024-03-14 19:54:47 +0100

    ccc

--- AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
+++ AjamiTv/src/main/kotlin/com/ajami/AjamiTvProvider.kt
@@ -12,9 +12,7 @@ import com.lagradost.cloudstream3.CommonActivity.activity
 import com.lagradost.cloudstream3.utils.UIHelper.navigate
 import com.lagradost.cloudstream3.R
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.core.type.TypeReference
+
 
 class AjamiTvProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://oha.to/play/"
@@ -47,20 +45,13 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
     if (channelsList.isEmpty()) {
         val wantedChannelNames = wantedChannels.map { it.name }
 
-        val req = app.get("https://oha.to/channels")
-        val mapper = jacksonObjectMapper()
-
-        val typeReference = object : TypeReference<List<OhaChannel>>() {}
-
-        val listOhaChannels: List<OhaChannel> = mapper.readValue(req.text, typeReference)
-
-
+        val listOhaChannels:List<OhaChannel> = app.get("https://oha.to/channels").parsedSafe<List<OhaChannel>>() ?: emptyList()
         val filtredChannels:List<Channel> = listOhaChannels
     .filter { it.name in wantedChannelNames }
     .mapNotNull { ohaChannel ->
         val wc = wantedChannels.find { it.name == ohaChannel.name }
         wc?.let {
-            Channel(ohaChannel.name ?: "", ohaChannel.id.toString(), ohaChannel.country ?: "", wc.poster, wc.group)
+            Channel(ohaChannel.name, ohaChannel.id.toString(), ohaChannel.country, wc.poster, wc.group)
         }
     }
     .filterNotNull()
@@ -136,9 +127,10 @@ class AjamiTvProvider : MainAPI() { // all providers must be an instance of Main
     )
 
     data class OhaChannel(
-        @JsonProperty("country") val country: String? = null,
-        @JsonProperty("id") val id: Long? = null,
-        @JsonProperty("name") val name: String? = null,
+        @JsonProperty("country") val country: String,
+        @JsonProperty("id") val id: Long,
+        @JsonProperty("name") val name: String,
+        @JsonProperty("p") val p: Int?=null
     )
 
 
